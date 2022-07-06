d6e8ebfcdfea4a8fc9fed1f82fc81a6865135413
wojciechguze2 <wojciech.guze@coloursfactory.pl>
2022-07-06 14:30:56 +0200

    w-6 - register page

--- django_shop/account/urls.py
+++ django_shop/account/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 
-from django_shop.account.views import LoginViewSet
+from django_shop.account.views import LoginViewSet, RegisterViewSet
 
-# products
 login = LoginViewSet.as_view({
     'get': 'login',
     'post': 'login',
 })
 
+register = RegisterViewSet.as_view({
+    'get': 'register',
+    'post': 'register',
+})
+
 urlpatterns = [
     path('login', login, name='front_login'),
+    path('register', register, name='front_register')
 ]
--- django_shop/account/views.py
+++ django_shop/account/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import redirect, render
 from rest_framework import viewsets
 from rest_framework.request import Request
 
+from django_shop.account.forms import RegisterForm
 from django_shop.globals.decorators import exceptions_debugger
 
 
@@ -15,20 +16,45 @@ class LoginViewSet(viewsets.ViewSet):
     @exceptions_debugger()
     def login(request: Request):
         if request.user.is_authenticated:
-            return redirect('front_homepage')
+            return redirect('front_homepage')  # todo: change to orders or account page
 
         if request.method == 'POST':
-            username = request.POST.get('username')  # email
+            email = request.POST.get('username')  # email
             password = request.POST.get('password')
 
-            user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=email, password=password)
 
             if user is not None:
                 login(request, user)
 
-                return redirect('front_account')
+                return redirect('front_homepage')
             else:
                 messages.info(request, 'Username or password is incorrect')
 
         return render(request, 'front_login.html', {})
 
+
+class RegisterViewSet(viewsets.ViewSet):
+    @staticmethod
+    @exceptions_debugger()
+    def register(request: Request):
+        if request.user.is_authenticated:
+            return redirect('front_homepage')  # todo: change to orders or account page
+
+        form = RegisterForm()
+
+        if request.method == 'POST':
+            form = RegisterForm(request.POST)
+
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('email')
+                messages.success(request, 'Account was created for ' + username)
+
+                return redirect('front_login')
+
+        template_variables = {
+            'form': form
+        }
+
+        return render(request, 'front_register.html', template_variables)
