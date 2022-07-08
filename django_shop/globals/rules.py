d074ce431559cbbf37e39acca840a430ec5a19fa
wojciechguze2 <wojciech.guze@coloursfactory.pl>
2022-07-08 23:16:02 +0200

    w-7 pagination class

--- django_shop/globals/rules.py
+++ django_shop/globals/rules.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Pagination:
     def __init__(self, queryset, request_page_number: int = 1, limit: int = 5):
         if isinstance(queryset, list):
@@ -6,24 +9,26 @@ class Pagination:
             self.queryset_count = queryset.count()
 
         self.limit = limit
-        self.pages_count = self.queryset_count // self.limit
+        self.pages_count = math.ceil(self.queryset_count / self.limit)
         self.page_number = self.get_page_number(request_page_number)
         self.offset = (self.page_number - 1) * self.limit
 
-        if self.page_number == -1:
-            self.page_results = queryset[(self.queryset_count + self.offset) * -1:]
+        if self.page_number == -1 and self.queryset_count % self.limit == 0:
+            self.page_results = queryset[self.limit * -1:]
+        elif self.page_number == -1 and self.queryset_count % self.limit != 0:
+            self.page_results = queryset[(self.queryset_count % self.limit) * -1:]
         else:
             self.page_results = queryset[self.offset:self.offset + self.limit]
 
         self.current = self.page_number
         self.previous = self.current - 1 if self.current - 1 > 0 else None
-        self.next = self.current + 1 if self.current <= self.pages_count else None
+        self.next = self.current + 1 if self.current < self.pages_count else None
 
     def get_json(self):
         if self.current == -1:  # last
             return {
-                'previous': self.pages_count,
-                'current': self.pages_count + 1,
+                'previous': self.pages_count - 1,
+                'current': self.pages_count,
                 'next': None
             }
         else:
